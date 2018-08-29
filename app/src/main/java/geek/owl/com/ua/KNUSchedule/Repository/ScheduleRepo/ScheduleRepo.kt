40fb36021cc28bec385e369a5af5d6f0d4f145a5
Viacheslav <slavcheg21071993@gmail.com>
2018-08-30 01:09:20 +0300

    Update

--- app/src/main/java/geek/owl/com/ua/KNUSchedule/Repository/ScheduleRepo/ScheduleRepo.kt
+++ app/src/main/java/geek/owl/com/ua/KNUSchedule/Repository/ScheduleRepo/ScheduleRepo.kt
@@ -1,6 +1,7 @@
 package geek.owl.com.ua.KNUSchedule.Repository.ScheduleRepo
 
 import android.arch.lifecycle.MutableLiveData
+import android.util.SparseArray
 import geek.owl.com.ua.KNUSchedule.Repository.DayPojo
 import geek.owl.com.ua.KNUSchedule.Repository.SchedulePojo
 import geek.owl.com.ua.KNUSchedule.Util.ApiService
@@ -9,6 +10,7 @@ import geek.owl.com.ua.KNUSchedule.Util.ErrorHandler
 import kotlinx.coroutines.experimental.launch
 
 class ScheduleRepo(val action: MutableLiveData<String>) {
+    val dayListLiveData = MutableLiveData<List<DayPojo>>()
 
     private val searchableData: MutableLiveData<List<SchedulePojo>> = MutableLiveData()
     private val apiService by lazy {
@@ -22,14 +24,24 @@ class ScheduleRepo(val action: MutableLiveData<String>) {
 
     fun getScheduleLiveData(group: String, week: Int): MutableLiveData<List<DayPojo>> {
         val job = launch {
-            val data1 = MutableLiveData<List<DayPojo>>()
+            val dayList = ArrayList<DayPojo>()
             val data =  database.getSchedule(group, week) as MutableLiveData<List<SchedulePojo>>
             data.value?.let { list ->
+                val dayMap = SparseArray<ArrayList<SchedulePojo>>()
                 list.forEach { item->
-
+                    if(dayMap.get(item.day)!=null){
+                        dayMap.get(item.day).add(item)
+                    }else dayMap.put(item.day, ArrayList())
                 }
+                for( i in 0 until dayMap.size())
+                    dayList.add(DayPojo(dayMap.keyAt(i)).also {
+                        it.scheduleList = dayMap.valueAt(i)
+                    })
             }
+            dayListLiveData.value = dayList
         }
+        
+        return dayListLiveData
     }
 
     fun searchSchedule(query: String, take: Int, offset: Int) {
